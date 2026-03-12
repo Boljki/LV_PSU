8741ff3836cfc124a892f055776e3ab9ae28c6f4
Boljki <domagojboljkivac@gmail.com>
2026-03-12 19:31:30 +0100

    Add files via upload

---
+++ LV1_Boljkovac/LV1_Boljkovac_zad1.py
@@ -0,0 +1,10 @@
+
+def print_hello(sati, cijena):
+    print("Ukupno:", placa, "eura")
+
+print("Radni sati")
+sati=float(input())
+print("eur/h")
+cijena=float(input())
+placa = sati * cijena
+print_hello(sati, cijena)
